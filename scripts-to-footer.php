acde5d025c0af9e0a6cdce9a56605b309ee739af
Joshua David Nelson <josh@joshuadnelson.com>
2014-05-01 18:46:49 -0700

    added conditional to avoid back-end conflicts

--- scripts-to-footer.php
+++ scripts-to-footer.php
@@ -12,7 +12,7 @@
  * Plugin Name: Scripts-To-Footer
  * Plugin URI: http://wordpress.org/plugins/scripts-to-footerphp/
  * Description: Moves scripts to the footer to decrease page load times, while keeping stylesheets in the header. Requires that plugins and theme correctly utilizes wp_enqueue_scripts hook. Can be disabled via a checkbox on specific pages and posts.
- * Version: 0.2
+ * Version: 0.3
  * Author: Joshua David Nelson
  * Author URI: http://joshuadnelson.com
  * License: GPL2
@@ -43,7 +43,7 @@ if( !defined( 'STF_DOMAIN' ) )
 
 // Plugin Verison
 if( !defined( 'STF_VERSION' ) )
-	define( 'STF_VERSION', '0.2' );
+	define( 'STF_VERSION', '0.3' );
 
 /**
  * Scripts to Footer Class.
@@ -99,7 +99,7 @@ class JDN_Scripts_To_Footer {
 	function clean_head() {
 		$excluded_pages = get_post_meta( get_queried_object_id(), 'stf_exclude', true );
 		
-		if( 'on' != $excluded_pages ) {
+		if( 'on' !== $excluded_pages && !is_admin() ) {
 			remove_action( 'wp_head', 'wp_print_scripts' ); 
 			remove_action( 'wp_head', 'wp_print_head_scripts', 9 ); 
 			remove_action( 'wp_head', 'wp_enqueue_scripts', 1 ); 
