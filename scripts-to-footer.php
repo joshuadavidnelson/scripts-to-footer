455c4a2cce215071e8f950103f054dfe75db47d0
Joshua Nelson <josh@joshuadnelson.com>
2025-11-16 17:40:39 -0800

    plugin requires at least 5.8, doc block update

--- scripts-to-footer.php
+++ scripts-to-footer.php
@@ -6,7 +6,7 @@
  * Plugin URI: http://wordpress.org/plugins/scripts-to-footerphp/
  * Description: Move your scripts to the footer to help speed up perceived page load times and improve user experience.
  * Version: 0.7.2
- * Requires at least: 4.8
+ * Requires at least: 5.3
  * Requires PHP: 7.4
  * Author: Joshua David Nelson
  * Author URI: http://joshuadnelson.com
